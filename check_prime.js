c7535400e7afdb7824c6e587eac1a58a267aa204
Shahid77137 <siddiquibabuddin202@gmail.com>
2022-09-20 19:31:57 +0530

    prime problem

---
+++ check_prime.js
@@ -0,0 +1,20 @@
+let num = 17;
+function check_prime(num){
+let factors = 0;
+for(let i=1;i<=N;i++){
+ if(num%i=0){
+  factors++;
+}
+}
+if(fac=0){
+ return true;
+}else{
+ return false 
+}
+}
+let result = check_prime(num)
+if(result==true){
+console.log(" prime")
+}else {
+console.log("not")
+}
